9d3a401956a65da1f11de24b78ab870c77e71a12
scheric <38077357+scheric@users.noreply.github.com>
2020-03-07 21:37:33 +0100

    add first sensor

--- dsmr-esp/constants.py
+++ dsmr-esp/constants.py
@@ -1,18 +1,17 @@
-"""constants for [xxxxx] esp """
+"""constants for dsmr-esp"""
 
 MQTT_TOPICS = [
-    "/ems-esp/boiler_data", #0
-    "/ems-esp/heartbeat",  #1
-    "/ems-esp/tapwater_active ", #2
+    "sensors/power/p1meter/", #0
+
 ]
     
 CONSTANTS_SENSORS_SENSORS = {
-    "active heating mode": {
-        "value": "temp_",
-        "topic": MQTT_TOPICS[8],
-        "name": "extra thermometer",
-        "icon": "mdi:thermometer-high",
-        "unit": None,
+    "low tarif consumption": {
+        "value": "consumption_low_tarif",
+        "topic": MQTT_TOPICS[0],
+        "name": "low tarif consumprion",
+        "icon": "flash",
+        "unit": kwh,
     },
 }
 
